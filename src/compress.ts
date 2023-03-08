226b1ebc33886078aec699feb003c953e6d10f4d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-03-08 08:13:15 +0800

    adjust color palette size and skip sub-region to compress each frame

--- src/compress.ts
+++ src/compress.ts
@@ -1,10 +1,13 @@
+import { writeFileSync } from 'fs'
+import jpeg from 'jpeg-js'
 import robot from 'robotjs'
 import zlib from 'zlib'
 import {
   applyPalette,
-  createPaletteKMean,
+  initPaletteKMean,
   getPalette,
   PaletteTable,
+  createEmptyPalette,
 } from './palette'
 import { h, offset, w } from './screenshot'
 
@@ -59,12 +62,13 @@ function test() {
   console.log('compressed', compressed.length.toLocaleString())
 
   console.time('prepare color palette')
-  let palette = createPaletteKMean(image)
+  let palette = createEmptyPalette(2) // preserve a slot of skip frame
+  initPaletteKMean(palette, image, 100)
   console.timeEnd('prepare color palette')
 
   let codes = Buffer.alloc(w * h)
   console.time('apply color palette')
-  let paletteTable: PaletteTable = new Array(256)
+  let paletteTable: PaletteTable = new Array(palette.length)
   let n = image.length
   for (let i = 0; i < n; i += 4) {
     let b = image[i + 0]
@@ -83,12 +87,102 @@ function test() {
   console.timeEnd('compress')
   console.log('compressed', compressed.length.toLocaleString())
 
-  for (let level = 1; level <= 9; level++) {
-    console.time('compress level ' + level)
-    let out = zlib.gzipSync(codes, { level })
-    console.timeEnd('compress level ' + level)
-    console.log('size', out.length.toLocaleString())
+  // for (let level = 1; level <= 9; level++) {
+  //   console.time('compress level ' + level)
+  //   let out = zlib.gzipSync(codes, { level })
+  //   console.timeEnd('compress level ' + level)
+  //   console.log('size', out.length.toLocaleString())
+  // }
+
+  function saveImage(file: string) {
+    let rawImage = Buffer.alloc(w * h * 4)
+    let c = 0
+    for (let i = 0; i < w * h * 4; i += 4, c++) {
+      let code = palette[codes[c]]
+      let r = (code >> 16) & 255
+      let g = (code >> 8) & 255
+      let b = (code >> 0) & 255
+      rawImage[i + 0] = r
+      rawImage[i + 1] = g
+      rawImage[i + 2] = b
+      rawImage[i + 3] = 255
+    }
+    let jpegImage = jpeg.encode({ width: w, height: h, data: rawImage })
+    writeFileSync(file, jpegImage.data)
   }
+
+  saveImage('distort-before.jpeg')
+
+  console.log(codes)
+  function blur() {
+    let distort = 0
+    for (; compressed.length > 65506; ) {
+      let radius = Math.ceil(Math.sqrt(compressed.length - 65506))
+      let batch = 1
+      for (let b = 0; b < batch; b++) {
+        distort++
+        let cx = Math.floor(Math.random() * w - radius * 2 + radius)
+        let cy = Math.floor(Math.random() * h - radius * 2 + radius)
+        let counts: number[] = []
+        for (let dy = -radius; dy <= radius; dy++) {
+          for (let dx = -radius; dx <= radius; dx++) {
+            let x = cx + dx
+            let y = cy + dy
+            let i = y * w + x
+            let c = codes[i]
+            counts[c] = (counts[c] || 0) + 1
+          }
+        }
+        let mode = +Object.entries(counts).sort((a, b) => b[1] - a[1])[0][0]
+        for (let dy = -radius; dy < radius; dy++) {
+          for (let dx = -radius; dx < radius; dx++) {
+            let x = cx + dx
+            let y = cy + dy
+            let i = y * w + x
+            codes[i] = mode
+          }
+        }
+      }
+      compressed = zlib.gzipSync(codes)
+      process.stdout.write(
+        `\r ${distort.toLocaleString()} distort | size ${compressed.length.toLocaleString()} `,
+      )
+    }
+    console.log()
+  }
+  function skip() {
+    let skip = 0
+    let min_radius = 100
+    for (; compressed.length > 65506; ) {
+      let radius = Math.ceil(Math.sqrt(compressed.length - 65506))
+      if (radius < min_radius) {
+        radius = min_radius
+      }
+      let batch = 1
+      for (let b = 0; b < batch; b++) {
+        skip++
+        let cx = Math.floor(Math.random() * w - radius * 2 + radius)
+        let cy = Math.floor(Math.random() * h - radius * 2 + radius)
+        for (let dy = -radius; dy < radius; dy++) {
+          for (let dx = -radius; dx < radius; dx++) {
+            let x = cx + dx
+            let y = cy + dy
+            let i = y * w + x
+
+            codes[i] = palette.length
+          }
+        }
+      }
+      compressed = zlib.gzipSync(codes)
+      process.stdout.write(
+        `\r skip ${skip.toLocaleString()} | size ${compressed.length.toLocaleString()} `,
+      )
+    }
+    console.log()
+  }
+  skip()
+
+  saveImage('distort-after.jpeg')
 }
 
-// test()
+test()
