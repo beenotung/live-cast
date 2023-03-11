9ef647abd23184450ab6ca7993e5c9484f01b8f4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-03-11 13:07:45 +0800

    turn off offset

--- src/screenshot.ts
+++ src/screenshot.ts
@@ -2,8 +2,8 @@
 import { exec, execSync } from 'child_process'
 import { max_size } from './config'
 
-// export let offset = [0, 0]
-export let offset = [1680, 0]
+export let offset = [0, 0]
+// export let offset = [1680, 0]
 // let offset = [640, 1178]
 
 export let [w, h] = [1920, 1080] // 5 fps
