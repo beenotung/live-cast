b2e13c121ec5a9585cfd2b866301f719098710ed
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-03-18 12:39:24 +0800

    label the alternative offset config

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ export let clientPort = 8457
 export let max_size = 65507
 
 export let offset = [0, 0]
-// export let offset = [1680, 0]
-// let offset = [640, 1178]
+// export let offset = [1680, 0] // right-side screen
+// export let offset = [253, 1211] // bottom screen
 
 export let [w, h] = [1920, 1080]
 
