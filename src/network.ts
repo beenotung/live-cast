f598911c99eda3ac11e2535d2fd7402003b6a381
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-11-16 12:10:14 +0800

    chore: treat private IPv4 ranges as local network

--- src/network.ts
+++ src/network.ts
@@ -13,6 +13,7 @@ function findAddress() {
       // |---------------|---------------|----------------|
       // | 192.168.1.100 | 255.255.255.0 | 192.168.1.255  |
       // | 172. 16.9.252 | 255.240.0.0   | 172.31.255.255 |
+      // | 10.0.0.1      | 255.0.0.0     | 10.255.255.255 |
       let address: (string | number)[] = iface.address.split('.')
       let netmask: (string | number)[] = iface.netmask.split('.')
 
@@ -25,12 +26,19 @@ function findAddress() {
     })
     .filter(address => address)
     .map(address => address!)
-  let broadcastAddress =
-    addresses.find(address => address.startsWith('192.168.')) ||
-    addresses.find(address => address.startsWith('172.31.')) ||
-    addresses.find(address => address.startsWith('192.'))
+  let broadcastAddress = addresses.find(address => isPrivateIPv4(address))
   if (!broadcastAddress) {
     throw new Error('failed to resolve broadcast address')
   }
   return broadcastAddress
+}
+
+// 192.168.0.0/16
+// 172.16.0.0/12
+// 10.0.0.0/8
+function isPrivateIPv4(ip: string) {
+  const [a, b] = ip.split('.').map(Number)
+  return (
+    a === 10 || (a === 172 && b >= 16 && b <= 31) || (a === 192 && b === 168)
+  )
 }
