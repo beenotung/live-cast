2f6c5c94a329a35503b4af663858c81a978e501f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-11-17 19:39:18 +0800

    chore: count the fps over time - to avoid getting higher number when messages arrives in brust

--- web-socket-version/src/fps.ts
+++ web-socket-version/src/fps.ts
@@ -1,35 +1,21 @@
 export function createFPSCounter() {
-  let lastTime = 0
-  let lastFPS = 0
-
-  let alpha = 0.9
-  let beta = 1 - alpha
+  let startTime = Date.now()
+  let count = 0
 
   function tick(): void {
-    let now = Date.now()
-    if (!lastTime) {
-      lastTime = now
-      return
-    }
-
-    let timePassed = now - lastTime || 1
-    lastTime = now
-    let fps = 1000 / timePassed
-
-    if (!lastFPS) {
-      lastFPS = fps
-      return
-    }
-
-    lastFPS = alpha * lastFPS + beta * fps
+    count++
   }
 
   return {
     tick,
-    getFPS: () => lastFPS,
+    getFPS: () => {
+      let now = Date.now()
+      let timePassed = (now - startTime) / 1000
+      return count / timePassed
+    },
     reset: () => {
-      lastTime = 0
-      lastFPS = 0
+      startTime = Date.now()
+      count = 0
     },
   }
 }
